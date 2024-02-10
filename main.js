b89f2e2c1f4dcc4de67fdbce6a1840ab308b658d
AlexWiraWilantapoera <alexwira99@gmail.com>
2024-02-10 11:37:12 +0700

    update form validation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// tab pages - START
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
 
@@ -16,10 +17,11 @@ function openCity(evt, cityName) {
 }
 
 document.getElementById("defaultOpen").click();
+// tab pages - END
 
-var nameInput = [];
-var age = [];
-var allowance = [];
+var list1 = [];
+var list2 = [];
+var list3 = [];
 
 var n = 1;
 var x = 0;
@@ -28,36 +30,20 @@ function AddRow() {
   var AddRown = document.getElementById("show");
   var NewRow = AddRown.insertRow(n);
 
-  let nameTemp = document.getElementById("name").value;
-  let ageTemp = document.getElementById("age").value;
-  let allowanceTemp = document.getElementById("allowance").value;
+  list1[x] = document.getElementById("name").value;
+  list2[x] = document.getElementById("age").value;
+  list3[x] = document.getElementById("allowance").value;
 
-  if (nameTemp.length >= 10 && ageTemp >= 25 && allowanceTemp >= 100000 && allowanceTemp <= 1000000) {
-    var cel1 = NewRow.insertCell(0);
-    var cel2 = NewRow.insertCell(1);
-    var cel3 = NewRow.insertCell(2);
+  var cel1 = NewRow.insertCell(0);
+  var cel2 = NewRow.insertCell(1);
+  var cel3 = NewRow.insertCell(2);
 
-    nameInput[x] = nameTemp;
-    age[x] = ageTemp;
-    allowance[x] = allowanceTemp;
+  cel1.innerHTML = list1[x];
+  cel2.innerHTML = list2[x];
+  cel3.innerHTML = list3[x];
 
-    cel1.innerHTML = nameInput[x];
-    cel2.innerHTML = age[x];
-    cel3.innerHTML = allowance[x];
-
-    n++;
-    x++;
-  } else {
-    if (nameTemp.length < 10) {
-      alert("Nama kurang dari 10 karakter.");
-    }
-    if (ageTemp < 25) {
-      alert("Usia kurang dari 25 tahun.");
-    }
-    if (allowanceTemp < 100000 && allowanceTemp > 1000000) {
-      alert("uang sangu harus berkisar antara 100000 dan 1000000.");
-    }
-  }
+  n++;
+  x++;
 }
 
 document.getElementById("resume").innerHTML = "Rata rata pendaftar memiliki uang sangu sebesar [nilai rata rata uang sangu] dengan rata rata umur [nilai rata rata umur]";
